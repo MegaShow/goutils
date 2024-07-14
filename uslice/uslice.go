b87147d17fd6c4ce9af36eb5f643d3d8286ed24a
MegaShow <a@icytown.com>
2024-07-14 15:53:25 +0800

    Add tuples and uobject.IsZero, IsNotZero (#3)

--- uslice/uslice.go
+++ uslice/uslice.go
@@ -3,17 +3,41 @@
 // 包 uslice 提供了切片相关的工具.
 package uslice
 
-// Filter create a new slice populated with the items that satisfy the provided function.
+// Distinct modify source slice and returns modified slice with distinct items.
 //
-// 创建一个新的数组, 这个数组由满足给定函数的元素组成.
-func Filter[T any](items []T, fn func(item T) bool) []T {
-	results := make([]T, 0, len(items))
-	for _, item := range items {
-		if fn(item) {
-			results = append(results, item)
+// 修改原来的数组, 确保数组每一个元素只存在一次, 并返回修改后的数组.
+func Distinct[T comparable](items []T) []T {
+	exists := make(map[T]struct{}, len(items))
+	i := 0
+	for k := 0; k < len(items); k++ {
+		_, ok := exists[items[k]]
+		if !ok {
+			if i != k {
+				items[i] = items[k]
+			}
+			i++
+			exists[items[k]] = struct{}{}
 		}
 	}
-	return results
+	clear(items[i:])
+	return items[:i]
+}
+
+// Filter modify source slice and returns modified slice with items that satisfy the provided function.
+//
+// 修改原来的数组, 确保数组每一个元素都满足给定函数条件, 并返回修改后的数组.
+func Filter[T any](items []T, fn func(item T) bool) []T {
+	i := 0
+	for k := 0; k < len(items); k++ {
+		if fn(items[k]) {
+			if i != k {
+				items[i] = items[k]
+			}
+			i++
+		}
+	}
+	clear(items[i:])
+	return items[:i]
 }
 
 // Map creates a new slice populated with the results of calling a provided function on every item in the calling array.
